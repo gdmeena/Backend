ab0e6a63b9610e36971ea58240f533a5182026be
Govardhan Meena <asakashmeena2005@gmail.com>
2025-06-13 21:47:09 +0000

    API response and error handling

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ import mongoose, { connect } from "mongoose"
 import { DB_NAME } from "./constants.js"  
 import connectDB from "./db/index.js"
 import  dotenv  from "dotenv"
+import { app } from "./app.js"
 
 
 dotenv.config({path:'.env'})
 connectDB()
+.then(()=>{
+app.listen(process.env.PORT ||8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
+})
+.catch((err)=>{
+    console.error("Error connecting to the database", err)
+    process.exit(1) // Exit the process with failure
+})
 
 
 
